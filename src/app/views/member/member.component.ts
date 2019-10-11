1a36b7f100a77023561999f482f8f3154eb860ca
Joseph Obligar <csa_jobligar@sjcoe.net>
2019-10-11 16:03:06 -0700

    added database

--- src/app/views/member/member.component.ts
+++ src/app/views/member/member.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../locale/interface/user';
 import { Challenge } from '../../locale/interface/challenge';
 import { CodeWarsData } from '../../locale/data';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'cwl-member',
@@ -14,14 +17,20 @@ export class MemberComponent implements OnInit {
   challenges: Challenge[] = [];
   userId: number;
   userName;
-  
-  constructor() { }
+  apiUrl = environment.apiUrl;
 
+
+  constructor(private http: HttpClient) { }
   ngOnInit() {
     this.userId = this.data.user[0].id;
     this.data.user.forEach(i => {
       this.users.push(i);
     });
     console.log(this.users);
+    this.http.get(this.apiUrl + '').toPromise().then(
+      res =>{
+        console.log(res);
+      }
+    )
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://codewarsapi20191010044451.azurewebsites.net/api/users'
 };
 
 /*
